730fc6a10415c2cffafa131783570f95c614b211
Michael Nulman <penguinus@gmail.com>
2024-07-24 16:43:19 +0300

    Fix for issue #27
    https://github.com/webbingbrasil/filament-copyactions/issues/27#issuecomment-1964712910

--- src/Forms/Actions/CopyAction.php
+++ src/Forms/Actions/CopyAction.php
@@ -14,9 +14,26 @@ class CopyAction extends BaseAction
     public function getCopyable(): ?string
     {
         if ($this->copyable === null) {
-            return $this->evaluate(fn ($component) => '$wire.'.$component->getStatePath());
+            return $this->evaluate(fn ($component) => '$wire.'.$this->changeFromDottedToBracket($component->getStatePath()));
         }
 
-        return $this->getDefaultCopyable();
+        return parent::getDefaultCopyable();
+    }
+
+    public function changeFromDottedToBracket($statePath): ?string{
+        if (strpos($statePath, '.') === false) {
+            return $statePath;
+        } 
+
+        $finalPath = "";
+        foreach (explode(".", $statePath) as $key => $value) {
+            if($value == "mountedTableActionsData"){
+                $finalPath = $finalPath.$value;
+            }else{
+                $finalPath = $finalPath."['".$value."']";
+            }
+        }
+
+        return $finalPath;
     }
 }
